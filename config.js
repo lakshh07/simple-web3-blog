40c12e735e1db2dbceb2226bb83c41879f0bbf24
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2022-05-30 14:04:17 +0530

    subgraph, smart contract and frontend

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-  
+export const contractAddress = "0x07F2F5573F17F0Fd794E19E71E6D8594C9769530";
+export const ownerAddress = "0x7b1C1702A09521b4160f79f853b7F54ba6b35a59";
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.10",
@@ -6,10 +7,12 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
-    // mumbai: {
-    //   url: "https://rpc-mumbai.matic.today",
-    //   accounts: [process.env.pk]
-    // },
+    mumbai: {
+      url: process.env.NEXT_PUBLIC_ALCHEMY_KEY,
+      accounts: [
+        `0x1a220a8b6c0c024deaebe717b7bd3440e283a17f1280a0683000dec9dd6ca356`,
+      ],
+    },
     // polygon: {
     //   url: "https://polygon-rpc.com/",
     //   accounts: [process.env.pk]
